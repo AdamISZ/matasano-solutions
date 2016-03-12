c0694df39e33962b1334a9ac304b0514a6ded63d
Adam Gibson <ekaggata@gmail.com>
2016-03-12 22:50:30 +0200

    tweaked 48 to expose multi-range code, all working, updated README, finished.

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # matasano-solutions
 
-My solutions for the cryptopals/matasano challenges in Python
+My solutions for the cryptopals/matasano challenges in Python, see www.cryptopals.com.
 
-Sets 1-6 (all challenges up to 46 so far, except for a couple that don't need code)
+Sets 1-6 finished (all challenges up to 48, except for a couple that don't need code).
 
 Included a slowaes.py AES implementation (with padding corrected)
+
 Shouldn't be any external dependencies (e.g. no OpenSSL), except for Crypto.RSA in a couple of later challenges, which is only used to generate primes, not to do RSA.
 
 There are a few detailed comments, especially on the ones that I found to be hard (not necessarily the ones they say are hard!)
 
-Nothing fancy here, just each challenge is one file, some reuse so if you want to run them just grab them all.
+Nothing fancy here, just each challenge is one file, some code reuse so if you want to run any of them just grab them all.
+
+I'm pretty much finished; there is a set 7 which I may look at in future.
+
+As far as I remember they all work.
--- challenges/matasano48.py
+++ challenges/matasano48.py
@@ -3,14 +3,10 @@
 import random
 import string
 import os
-import math
-from hashlib import sha1
 from matasano39 import encrypt, decrypt, inv
 from matasano47 import get_small_rsa_key, pkcs1v15_oracle, pkcs1v15_pad_for_encryption
 from matasano18 import bi2ba
 import binascii
-import base64
-from itertools import combinations
 
 
 def get_ranges(s, M, B, n):
@@ -37,16 +33,33 @@ pmsg = pkcs1v15_pad_for_encryption(msg, bl=bitlength)
 c = encrypt(n, e, pmsg, bitlength, outfmt='int')
 
 #Bleich. '98 full version
+#========================
 #Step 1 is skipped as we work with a ciphertext.
 B = 2**(bitlength-16)
-s = int(n/(3*B))
+
+#Important note: choosing a starting value of n/(3B) as suggested by Bleich.
+#yields a result just as fast (faster?) but seems to result
+#in a vanishingly small probability of having to execute step
+#2b; since multiple ranges only occur if s is large enough to
+#multiply the range (2B,3B) into a range spanning more than 
+#one modulus wrap. Hence we set it to a much larger value here
+#to make that become highly likely, thus "exercising" the 2b
+#step and making sure it works.
+s = int(5*n/(B))
+
+#the initial range corresponding to 00 02 initial bytes:
 M = [(2*B, 3*B-1)]
-i=0
+starting = True
+first_only1_range = True
+print """Starting the Bleichernbacher '98 attack, note that
+it takes anywhere from 30 seconds to several minutes on
+my machine, and chews (one) CPU..."""
+
 while True:
-    i+=1
-    if i==1:
-        #Step 2a. Find a s1 that is pkcs conformant. TODO Y so slow.
-        print "Preparing first s value, can take a minute or two.."
+    if starting:
+        starting = False
+        #Step 2a. Find a s1 that is pkcs conformant.
+        print "Preparing first s value..."
         while True:
             encs = encrypt(n, e, s, bitlength)
             c1 = c * encs % n
@@ -57,7 +70,8 @@ while True:
             s += 1
     else:
         if len(M)>1:
-            #Step 2b; isn't this just the same as 2a but with a different starting s?
+            #Step 2b
+            s += 1
             while True:
                 encs = encrypt(n, e, s, bitlength)
                 c1 = c * encs % n
@@ -83,6 +97,12 @@ while True:
                 #note that here 's' will be the final successful value
     #Step 3.
     M = get_ranges(s, M, B, n)
+    if len(M)>1:
+        print "got " + str(len(M)) + " ranges, continuing.."
+    else:
+        if first_only1_range:
+            print "only one range, starting the final narrowing down.."
+            first_only1_range = False
     #Step 4
     if len(M)==1 and M[0][0]==M[0][1]:
         print "cracked the message, it's:"
